9b21be53723a97deb772b572fa9a53aa02aef0f5
William Y. Feng <womogenes2@gmail.com>
2023-10-01 10:25:27 -0700

    Package state into one variable

--- js/index.js
+++ js/index.js
@@ -6,37 +6,38 @@ const main = () => {
 document.addEventListener('alpine:init', () => {
   console.log('Alpine initialized.');
 
-  const defaultTime = 5;
-  Alpine.store('time', defaultTime);
+  const defaultTime = 10;
+  Alpine.store('time', {
+    time: defaultTime,
+    totalTime: defaultTime,
+    get proportion() {
+      return 1 - this.time / this.totalTime;
+    },
+    get date() {
+      return convertTime(this.proportion);
+    },
+  });
+  const timeStore = Alpine.store('time');
   Alpine.store('timerInput', defaultTime);
-  Alpine.store('totalTime', defaultTime);
-  Alpine.store('proportion', 0);
-  Alpine.store('date', '...');
 
   const timer = new Timer({
     tick: 0.01,
     ontick: (ms) => {
-      Alpine.store('time', ms / 1000);
-      const p = 1 - ms / 1000 / Alpine.store('totalTime');
-      Alpine.store('proportion', p);
-      Alpine.store('date', convertTime(p).toLocaleString());
+      timeStore.time = ms / 1000;
     },
     onend: () => {
-      Alpine.store('time', 0);
-      Alpine.store('proportion', 1);
+      timeStore.time = 0;
     },
   });
   timer.start(defaultTime).pause();
 
   window.setTimer = () => {
-    const amount = parseFloat(Alpine.store('timerInput'));
+    const amount = Alpine.store('timerInput');
     if (!amount) return;
     timer.stop();
     timer.start(amount).pause();
-    Alpine.store('time', amount);
-    Alpine.store('totalTime', amount);
-    Alpine.store('proportion', 0);
-    Alpine.store('date', '...');
+    timeStore.time = amount;
+    timeStore.totalTime = amount;
   };
   window.pauseTimer = () => {
     if (timer.getStatus() === 'stopped') return;
--- js/time.js
+++ js/time.js
@@ -2,17 +2,36 @@
   Convert epoch numbers to readable dates
 */
 
+// https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+Number.prototype.toHHMMSS = function () {
+  var hours = Math.floor(this / 3600);
+  var minutes = Math.floor((this - hours * 3600) / 60);
+  var seconds = (this - hours * 3600 - minutes * 60).toFixed(2);
+
+  if (hours < 10) hours = '0' + hours;
+  if (minutes < 10) minutes = '0' + minutes;
+  if (seconds < 10) seconds = '0' + seconds;
+  return `${hours}:${minutes}:${seconds}`;
+};
+
 const convertTime = (p) => {
   // p is a fraction between 0 and 1
   const yearsBack = Math.exp(20.3444 * Math.pow(p, 3) + 3) - Math.exp(3);
-  const date = new Date(new Date() - yearsBack * 365.25 * 24 * 60 * 60 * 1000);
+  const epoch = new Date() - yearsBack * 365.25 * 24 * 60 * 60 * 1000;
 
+  if (yearsBack >= 2e5) {
+    return `${yearsBack.toPrecision(3)} years ago`;
+  }
+
+  const date = new Date(epoch);
   const options = {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
+    second: 'numeric',
+    fractionalSecondDigits: 3,
     hour12: true,
   };
   return new Intl.DateTimeFormat('en-US', options).format(date);
